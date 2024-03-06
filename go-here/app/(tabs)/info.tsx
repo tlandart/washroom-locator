593beca796838c0b475c0bb6a32ed88a4501f5ac
TrollingNewbie <leonhu518@gmail.com>
2024-03-05 23:01:34 -0500

    test commit 1

--- go-here/app/(tabs)/info.tsx
+++ go-here/app/(tabs)/info.tsx
@@ -7,6 +7,7 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Info</Text>
+      xxx
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       
     </View>
