76ab0e7e52859f617580f9094c11147f0f9c4c3f
TrollingNewbie <leonhu518@gmail.com>
2024-03-05 23:40:00 -0500

    New donate button to redirect user to donate page

--- go-here/app/(tabs)/info.tsx
+++ go-here/app/(tabs)/info.tsx
@@ -1,15 +1,15 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet} from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import DonateButton from '@/components/DonateButton';
+
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Info</Text>
-      xxx
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
+      <DonateButton />
     </View>
   );
 }
