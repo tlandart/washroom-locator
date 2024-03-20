87c716c0ad707713480a43b127ecb045fa6786ca
TrollingNewbie <leonhu518@gmail.com>
2024-03-19 23:18:20 -0400

    proper branch

---
+++ go-here/app/admin.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Admin</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 30,
+  },
+});
