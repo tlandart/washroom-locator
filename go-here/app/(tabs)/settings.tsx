0f2f1df29b1b9e2d567e2469449fb623ca662aed
TrollingNewbie <leonhu518@gmail.com>
2024-03-29 16:35:49 -0400

    Updated UI

--- go-here/app/(tabs)/settings.tsx
+++ go-here/app/(tabs)/settings.tsx
@@ -62,6 +62,7 @@ export default function SettingsScreen() {
       </TouchableOpacity> 
 
       <Text style={styles.title}>Admin</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {showLogin ? (
         <>
           <TextInput
