78149111e5ba1f4eef73c3d89506e490ea6b2432
Ethan Lam <el.at.tor@gmail.com>
2024-03-29 17:12:30 -0400

    Modified UI in settings page

--- go-here/app/(tabs)/settings.tsx
+++ go-here/app/(tabs)/settings.tsx
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginTop: 30,
+    marginTop: 50,
     color: Colors['light'].tint,
   },
   separator: {
